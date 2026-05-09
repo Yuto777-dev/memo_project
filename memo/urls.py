fe41b2f9c09536c51256c9ce30c42aa6dafe7eb0
katoyuto <fm2f8x6m9j@privaterelay.appleid.com>
2026-05-09 15:18:09 +0900

    フォーム完了

--- memo/urls.py
+++ memo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MemoListView
+from .views import MemoListView,MemoCreateView
 
 urlpatterns = [
-    path('',MemoListView.as_view(), name = 'index'),
+    path('', MemoListView.as_view(), name = 'index'),
+    path('new/', MemoCreateView.as_view(), name='create'),
 ]
--- memo/views.py
+++ memo/views.py
@@ -1,6 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
 from .models import Memo
 
 class MemoListView(ListView):
     model = Memo
     temlate_name = 'memo/memo_list.html'
+
+class MemoCreateView(CreateView):
+    model = Memo
+    tempate_name = 'memo/memo_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('index')
