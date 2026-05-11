f6e3664e7b338932bcddea527e368a2e3c15b831
katoyuto <fm2f8x6m9j@privaterelay.appleid.com>
2026-05-11 21:03:24 +0900

    編集機能（EDIT）実装完了

--- memo/urls.py
+++ memo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MemoListView,MemoCreateView
+from .views import MemoListView,MemoCreateView,MemoUpdateView
 
 urlpatterns = [
     path('', MemoListView.as_view(), name = 'index'),
     path('new/', MemoCreateView.as_view(), name='create'),
+    path('<int:pk>/edit/', MemoUpdateView.as_view(), name='memo-edit')
 ]
--- memo/views.py
+++ memo/views.py
@@ -1,14 +1,21 @@
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView
 from django.urls import reverse_lazy
 from .models import Memo
 from .form import MemoForm
 
 class MemoListView(ListView):
     model = Memo
-    temlate_name = 'memo/memo_list.html'
+    template_name = 'memo/memo_list.html'
 
 class MemoCreateView(CreateView):
     model = Memo
     form_class = MemoForm
     tempate_name = 'memo/memo_form.html'
     success_url = reverse_lazy('index')
+
+class MemoUpdateView(UpdateView):
+    model = Memo
+    form_class = MemoForm
+    template_name = 'memo/memo_form.html'
+    success_url = reverse_lazy('index')
+
